19b55e29156b5032f8659ffe3244323cce000b7d
Hans Harck Tønning <developer@hansit.dk>
2022-03-10 17:03:27 +0100

    Removes adding a negative amount of points

--- addpoint.php
+++ addpoint.php
@@ -262,8 +262,8 @@ if (isset($_POST['submit']) && ($_POST['submit'] == "Submit")  && ($CAPTCHA_succ
 
     $user = new bruger($studienr);
     $points = trim($points);
-    if (! (is_numeric($points))){
-        exit("Error:non integer point value!");
+    if (! (is_numeric($points)) || $points > 0){
+        exit("Error:non positive integer point value!");
     }
 
     $user->addpoint($points, $aktivitet, $kommentar, $dato, $approved = $approved_status);
